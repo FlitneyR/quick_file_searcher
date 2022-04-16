2d72261a061294243be3711e449c0877db1e247e
Robert Flitney <psyrf2@nottingham.ac.uk>
2022-04-16 21:53:45 +0100

    Add WordBitMapRow struct

--- src/searcher/processor.rs
+++ src/searcher/processor.rs
@@ -1,6 +1,56 @@
 use std::fs;
 use std::io::Read;
 
+/// Bitmap to record which words are used in which files
+pub struct WordBitMapRow {
+    bytes: Vec<u8>
+}
+
+impl WordBitMapRow {
+    /// Gets the value of a bit in the bitmap
+    pub fn get_bit(&self, index: usize) -> Option<bool> {
+        if index >= self.bytes.len() * 8 { None } else {
+            let bit = index % 8;
+
+            self
+            .get_byte(index / 8)
+            .and_then(|byte|
+                Some(byte & (1 << bit) > 0)
+            )
+        }
+    }
+
+    /// Sets the value of a bit in the bitmap
+    /// Returns true if successful, otherwise false
+    pub fn set_bit(&mut self, index: usize, val: bool) -> bool {
+        let byte_index = index / 8;
+        let bit = index % 8;
+
+        let value = if !val { 0 } else { 1 << bit };
+        let mask = !value;
+
+        let old_byte = self.get_byte(byte_index);
+        let new_byte = old_byte.and_then(|byte| Some((byte & mask) | value));
+        new_byte.and_then(|byte|
+            Some(self.set_byte(byte_index, byte))
+        ).unwrap_or(false)
+    }
+
+    /// Returns some byte at the specified index, if it exists, otherwise none
+    pub fn get_byte(&self, index: usize) -> Option<&u8> {
+        self.bytes.get(index)
+    }
+
+    /// Sets a byte
+    /// Returns true if successful, false otherwise
+    pub fn set_byte(&mut self, index: usize, val: u8) -> bool {
+        if index >= self.bytes.len() { false } else {
+            self.bytes[index] = val;
+            true
+        }
+    }
+}
+
 /// Returns a vector of filenames in the current directory
 pub fn get_paths() -> Vec<String> {
     fs::read_dir("./")
