a06e03302c65084a778962f2c88b09b781eab134
Robert Flitney <psyrf2@nottingham.ac.uk>
2022-04-22 16:51:53 +0100

    add installer script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+rm /usr/local/bin/search
+
+cargo build --release
+
+echo "Copying binary to /usr/local/bin/search"
+cp target/release/searcher /usr/local/bin/search
+echo "search was installed"
+
